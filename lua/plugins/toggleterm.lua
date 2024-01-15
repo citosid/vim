77b292b030fddf592a90e63eb1cdd0e7fc658875
Abraham Cruz Sustaita <abraham.sustaita@gmail.com>
2024-01-14 21:59:30 -0500

    fix(toggleterm): add C-j,C-j command to hide terminal

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,18 +1,28 @@
 return {
 	"akinsho/toggleterm.nvim",
 	cmd = { "ToggleTerm", "TermExec" },
-	opts = {
-		size = 10,
-		on_create = function()
-			vim.opt.foldcolumn = "0"
-			vim.opt.signcolumn = "no"
-		end,
-		open_mapping = [[<F7>]],
-		shading_factor = 2,
-		direction = "float",
-		float_opts = {
-			border = "curved",
-			highlights = { border = "Normal", background = "Normal" },
-		},
-	},
+	config = function()
+		function _G.set_terminal_keymaps()
+			local opts = { buffer = 0 }
+			vim.keymap.set("t", "<C-j><C-j>", [[<Cmd>wincmd h<CR>]], opts)
+		end
+
+		-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+		vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+
+		require("toggleterm").setup({
+			size = 10,
+			on_create = function()
+				vim.opt.foldcolumn = "0"
+				vim.opt.signcolumn = "no"
+			end,
+			open_mapping = [[<F7>]],
+			shading_factor = 2,
+			direction = "float",
+			float_opts = {
+				border = "curved",
+				highlights = { border = "Normal", background = "Normal" },
+			},
+		})
+	end,
 }
