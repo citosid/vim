0a94768e724a6c2e95759899cfa7fbce5c22461d
Abraham Cruz Sustaita <abraham.sustaita@gmail.com>
2024-06-18 23:43:31 -0400

    chore: small changes to vim
    - add new lualine
    - revert to rose pine theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -47,13 +47,20 @@ return {
 				},
 			})
 
-			-- vim.cmd("colorscheme rose-pine-moon")
+			vim.cmd("colorscheme rose-pine-moon")
 		end,
 	},
 	{
 		"samharju/synthweave.nvim",
+		enabled = false,
 		config = function()
-			vim.cmd("colorscheme synthweave-transparent")
+			-- vim.cmd("colorscheme synthweave-transparent")
+		end,
+	},
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup()
 		end,
 	},
 }
