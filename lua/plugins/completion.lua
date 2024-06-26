d3771e6f65c8b9721db5491f99f2377cc3dd53ca
Abraham Cruz Sustaita <abraham.sustaita@gmail.com>
2024-06-26 14:28:52 -0400

    fix: fix luasnip errors

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -54,14 +54,14 @@ return {
 					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 			end
 
-			require("luasnip.loaders.from_vscode").lazy_load()
+			-- require("luasnip.loaders.from_vscode").lazy_load()
 			local cmp = require("cmp")
 			-- local luasnip = require("luasnip")
 
 			cmp.setup({
 				snippet = {
 					expand = function(args)
-						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+						-- require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
 					end,
 				},
 				window = {
