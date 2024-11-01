cd5bfd6c63bb76ec2510876d0ddf029256795c65
Abraham Cruz Sustaita <abraham.sustaita@gmail.com>
2024-11-01 11:53:39 -0400

    feat(vim): add signs icons

--- lua/options.lua
+++ lua/options.lua
@@ -74,3 +74,9 @@ end
 
 -- Fix markdown indentation settings
 vim.g.markdown_recommended_style = 0
+
+-- Signs
+vim.fn.sign_define("DiagnosticSignError", { text = "⛔", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = "󱍼", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "💡", texthl = "DiagnosticSignHint" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = "💁", texthl = "DiagnosticSignInfo" })
