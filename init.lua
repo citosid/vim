047f905f5a5dbc27b6bb4b16d0c267d6c94b8367
Abraham Cruz Sustaita <abraham.sustaita@gmail.com>
2024-11-24 01:16:53 -0500

    feat(time): add load time alert when vim finishes loading

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+local start_time = vim.loop.hrtime()
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
@@ -21,3 +22,9 @@ require("lazy").setup({
 })
 require("keymaps")
 require("autocmds")
+
+local end_time = vim.loop.hrtime()
+local elapsed_ms = (end_time - start_time) / 1e6 -- Convert nanoseconds to milliseconds
+vim.defer_fn(function()
+	vim.notify(string.format("Neovim loaded in %.2f ms", elapsed_ms), vim.log.levels.INFO)
+end, 100)
