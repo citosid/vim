6f6dd5927a5bfa033413591add9e96a5dd4fd790
Abraham Cruz Sustaita <abraham.sustaita@gmail.com>
2024-11-25 21:41:18 -0500

    fix(vim): disable supermaven for markdown files

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -59,12 +59,13 @@ return {
 		event = "BufReadPre",
 		config = function()
 			require("supermaven-nvim").setup({
+				disable_keymaps = false,
+				ignore_filetypes = { markdown = true },
 				keymaps = {
 					accept_suggestion = "<C-y>",
 					clear_suggestion = "<C-]>",
 					accept_word = "<C-j>",
 				},
-				disable_keymaps = false,
 			})
 		end,
 	},
