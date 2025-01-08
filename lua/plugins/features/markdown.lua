8a5a563b38838b51e288ffe07591d5b4da2bdd60
Abraham Cruz Sustaita <abraham.sustaita@gmail.com>
2025-01-08 09:09:48 -0500

    chore: add functionality to preview images in markdown

--- lua/plugins/features/markdown.lua
+++ lua/plugins/features/markdown.lua
@@ -1,10 +1,29 @@
 return {
-	"MeanderingProgrammer/render-markdown.nvim",
-	opts = {
-		file_types = { "markdown", "Avante" },
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		opts = {
+			file_types = { "markdown", "Avante" },
+		},
+		ft = { "markdown", "Avante" },
+		config = function()
+			require("render-markdown").setup({})
+		end,
 	},
-	ft = { "markdown", "Avante" },
-	config = function()
-		require("render-markdown").setup({})
-	end,
+	{
+		"3rd/image.nvim",
+		config = function()
+			require("image").setup({
+				integrations = {
+					markdown = {
+						enabled = true,
+						clear_in_insert_mode = true,
+						download_remote_images = true,
+						only_render_image_at_cursor = true,
+						floating_windows = false, -- if true, images will be rendered in floating markdown windows
+						filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+					},
+				},
+			})
+		end,
+	},
 }
