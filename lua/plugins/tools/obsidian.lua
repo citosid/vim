313fb0e5a076487762ccfe1765ad4b38591be0e8
Abraham Cruz Sustaita <abraham.sustaita@gmail.com>
2025-01-15 00:14:18 -0500

    chore: spell

--- lua/plugins/tools/obsidian.lua
+++ lua/plugins/tools/obsidian.lua
@@ -7,20 +7,8 @@ return {
 	opts = {
 		workspaces = {
 			{
-				name = "bible-study",
-				path = "~/code/personal/bible-study/",
-			},
-			{
-				name = "letters",
-				path = "~/code/personal/letters/",
-			},
-			{
 				name = "notes",
 				path = "~/code/personal/notes/",
-			},
-			{
-				name = "watchtower_study",
-				path = "~/code/personal/watchtower_study/",
 			},
 		},
 	},
