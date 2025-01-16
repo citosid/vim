40dbd1abae3442d4b3e94c350eb5b47dda37f03e
Abraham Cruz Sustaita <abraham.sustaita@gmail.com>
2025-01-16 07:57:31 -0500

    fix(image.nvim): improve image previewer configuration so they don't overlap windows

--- lua/plugins/features/markdown.lua
+++ lua/plugins/features/markdown.lua
@@ -16,10 +16,9 @@ return {
 				integrations = {
 					markdown = {
 						enabled = true,
-						clear_in_insert_mode = false,
+						clear_in_insert_mode = true,
 						download_remote_images = true,
-						only_render_image_at_cursor = false,
-						floating_windows = false, -- if true, images will be rendered in floating markdown windows
+						only_render_image_at_cursor = true,
 						filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
 					},
 				},
