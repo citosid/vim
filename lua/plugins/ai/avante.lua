7cd5e90d48e54bb02f32c8b91fb1b487e690703f
Abraham Cruz Sustaita <abraham.sustaita@gmail.com>
2025-01-31 15:35:56 -0500

    feat(avante): add avante to use copilot chat in vim

---
+++ lua/plugins/ai/avante.lua
@@ -0,0 +1,38 @@
+return {
+	{
+		"yetone/avante.nvim",
+		lazy = true,
+		cmd = { "AvanteAsk", "AvanteChat", "AvanteToggle" },
+		version = "*", -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+		opts = {
+			provider = "copilot",
+			behaviour = {
+				auto_suggestions = false,
+			},
+		},
+		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+		build = "make",
+		dependencies = {
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			{
+				-- support for image pasting
+				"HakonHarnes/img-clip.nvim",
+				event = "VeryLazy",
+				opts = {
+					-- recommended settings
+					default = {
+						embed_image_as_base64 = false,
+						prompt_for_file_name = false,
+						drag_and_drop = {
+							insert_mode = true,
+						},
+						-- required for Windows users
+						use_absolute_path = true,
+					},
+				},
+			},
+		},
+	},
+}
