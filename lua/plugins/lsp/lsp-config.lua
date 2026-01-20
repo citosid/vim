842da78ec6b24ad2369c1e4199cc777181d99c89
Abraham Cruz Sustaita <abraham.sustaita@gmail.com>
2026-01-19 19:28:19 -0500

    fix(lsp): 🐛 adjust diagnostic float keymap to <space>[e

--- lua/plugins/lsp/lsp-config.lua
+++ lua/plugins/lsp/lsp-config.lua
@@ -56,7 +56,7 @@ return {
 			-- vim.lsp.enable("harper_ls")
 
 			-- Global mappings.
-			vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+			vim.keymap.set("n", "<space>[e", vim.diagnostic.open_float)
 			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
 			vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
 			vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
