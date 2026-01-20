3a4d3cb0e2660ebb6d2dc2820769827f2a46f658
Abraham Cruz Sustaita <abraham.sustaita@gmail.com>
2026-01-19 19:10:39 -0500

    chore(ui): 🎨 switch catppuccin to frappe flavour

--- lua/plugins/ui/catppuccin.lua
+++ lua/plugins/ui/catppuccin.lua
@@ -3,13 +3,13 @@ return {
 	name = "catppuccin",
 	event = { "UIEnter" },
 	opts = {
-		flavour = "mocha",
+		flavour = "frappe",
 		term_colors = true,
 		transparent_background = true,
 	},
 	priority = 1000,
 	config = function(_, opts)
 		require("catppuccin").setup(opts)
-		vim.cmd.colorscheme("catppuccin-mocha")
+		vim.cmd.colorscheme("catppuccin-frappe")
 	end,
 }
