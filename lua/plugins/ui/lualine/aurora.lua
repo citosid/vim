03bdd097bce567539b061fe19bb7573167111fa2
Abraham Cruz Sustaita <abraham.sustaita@gmail.com>
2026-02-19 11:12:08 -0500

    style(colors): 🎨 update aurora color palette

--- lua/plugins/ui/lualine/aurora.lua
+++ lua/plugins/ui/lualine/aurora.lua
@@ -1,17 +1,17 @@
 -- Aurora lualine theme - auto-generated from wallpaper
 
 local colors = {
-	bg = "#0d1110",
-	bg_light = "#1a2220",
-	fg = "#e0e8f0",
-	fg_dark = "#a0b0c0",
-	coral = "#f05a5a",
-	teal = "#169c7b",
-	orange = "#e8a54a",
-	magenta = "#c44a8a",
-	cyan = "#0bd9a6",
-	blue = "#4a8ac4",
-	gray = "#4a6a8a",
+	bg = "#071417",
+	bg_light = "#142225",
+	fg = "#d7e0e2",
+	fg_dark = "#98a0a2",
+	coral = "#ce7cc8",
+	teal = "#00aa97",
+	orange = "#bab338",
+	magenta = "#8994f4",
+	cyan = "#00c7d6",
+	blue = "#10ace6",
+	gray = "#496972",
 	none = "NONE",
 }
 
