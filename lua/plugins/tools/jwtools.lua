723f77f2f3221bda5c5239017d43892dda828131
Abraham Cruz Sustaita <abraham.sustaita@gmail.com>
2026-04-04 01:05:19 -0600

    feat(jwtools): add lazy triggers for notes search and tag pickers
    Add <leader>jsn, <leader>jt keymaps and new user commands to lazy
    spec. Add telescope.nvim as dependency.

--- lua/plugins/tools/jwtools.lua
+++ lua/plugins/tools/jwtools.lua
@@ -2,13 +2,19 @@ return {
 	{
 		cmd = {
 			"JWToolsFetchScripture",
+			"JWToolsSearchNotes",
+			"JWToolsInsertTag",
+			"JWToolsRefreshTags",
 		},
 		config = function()
 			require("jwtools").setup()
 		end,
+		dependencies = { "nvim-telescope/telescope.nvim" },
 		dir = "~/code/personal/jwtools.nvim",
 		keys = {
-			{ "<leader>jf", "<cmd>JWToolsFetchScripture<cr>" },
+			{ "<leader>jf", "<cmd>JWToolsFetchScripture<cr>", desc = "JW: Fetch scripture" },
+			{ "<leader>jsn", desc = "JW: Search notes" },
+			{ "<leader>jt", desc = "JW: Insert tag" },
 		},
 	},
 }
